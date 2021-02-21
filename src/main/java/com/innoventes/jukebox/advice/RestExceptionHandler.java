5432360e80a57416109110c4f66e7453bc1b7914
Emil Pradeep <emilpradeep@gmail.com>
2021-02-22 00:23:54 +0530

    cleaned up code

--- src/main/java/com/innoventes/jukebox/advice/RestExceptionHandler.java
+++ src/main/java/com/innoventes/jukebox/advice/RestExceptionHandler.java
@@ -1,38 +1,19 @@
 package com.innoventes.jukebox.advice;
 
 import com.innoventes.jukebox.models.response.ErrorResponse;
-import com.innoventes.jukebox.models.response.JukeboxResponse;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.validation.ConstraintViolation;
-import javax.validation.ConstraintViolationException;
-import java.util.ArrayList;
-import java.util.Set;
 
 @Order(Ordered.HIGHEST_PRECEDENCE)
 @ControllerAdvice
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
-
-//    @ResponseStatus(HttpStatus.BAD_REQUEST)
-//    @ExceptionHandler({HttpMessageNotReadableException.class, MethodArgumentNotValidException.class,
-//            HttpRequestMethodNotSupportedException.class})
-//    public ResponseEntity<JukeboxResponse> handle(HttpServletRequest req, Exception exception) {
-////        Set<ConstraintViolation<?>> errorMessage = exception.getConstraintViolations();
-//        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ErrorResponse(exception.getMessage()));
-//    }
 
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
