5869039b445db49654c7ec54a96a2dd0e0b1254e
Emil Pradeep <emilpradeep@gmail.com>
2021-02-21 18:56:25 +0530

    configured mysql database and db migration configuration

---
+++ src/main/resources/db/migration/V1__Initial.sql
@@ -0,0 +1,2 @@
+CREATE TABLE tbl_users(id INT AUTO_INCREMENT PRIMARY KEY, email VARCHAR(255), password VARCHAR(255));
+INSERT INTO tbl_users(email, password) VALUES ('admin@jukebox.com', 'password');
