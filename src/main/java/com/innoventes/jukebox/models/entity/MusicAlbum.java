78ecf69b7cc2e80829bcb4c7b404d2a65fc716fe
Emil Pradeep <emilpradeep@gmail.com>
2021-02-21 22:33:24 +0530

    added entity relationships

---
+++ src/main/java/com/innoventes/jukebox/models/entity/MusicAlbum.java
@@ -0,0 +1,87 @@
+package com.innoventes.jukebox.models.entity;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity(name = "tbl_music_album")
+public class MusicAlbum {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "genre")
+    private String genre;
+
+    @Column(name = "price")
+    private Integer price;
+
+    @Column(name = "description")
+    private String description;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "album_musicians",
+            joinColumns = {@JoinColumn(name = "album_id", referencedColumnName = "id")},
+            inverseJoinColumns = {@JoinColumn(name = "musician_id", referencedColumnName = "id")}
+    )
+    private Set<Musician> musician;
+
+    public MusicAlbum(String name, String genre, Integer price, String description) {
+        this.name = name;
+        this.genre = genre;
+        this.price = price;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Set<Musician> getMusician() {
+        return musician;
+    }
+
+    public void setMusician(Set<Musician> musician) {
+        this.musician = musician;
+    }
+}
---
+++ src/main/java/com/innoventes/jukebox/models/entity/Musician.java
@@ -0,0 +1,46 @@
+package com.innoventes.jukebox.models.entity;
+
+import javax.persistence.*;
+
+@Entity(name = "tbl_musician")
+public class Musician {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "type")
+    private String type;
+
+    @Column(name = "name")
+    private String name;
+
+    public Musician(String type, String name) {
+        this.type = type;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
