636f50aaf991178f3e90d9464f7d761e6518808d
Emil Pradeep <emilpradeep@gmail.com>
2021-02-23 22:38:01 +0530

    bug fixed

--- src/main/java/com/innoventes/jukebox/repository/AlbumRepository.java
+++ src/main/java/com/innoventes/jukebox/repository/AlbumRepository.java
@@ -6,8 +6,12 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
+
 public interface AlbumRepository extends JpaRepository<MusicAlbum, Long> {
 
     @Query("SELECT a FROM tbl_music_album a ORDER BY price")
     Page<MusicAlbum> fetchMusicAlbums(Pageable pageable);
+
+
 }
