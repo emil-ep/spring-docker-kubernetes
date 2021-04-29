bd698c1e75857ec21988a511d61889243083c05e
Emil <emilpradeep@gmail.com>
2021-04-29 18:42:10 +0530

    added more test for authentication controllers. Added TestAuthHelper class to generate token

--- src/test/java/com/innoventes/jukebox/controller/AbstractControllerTest.java
+++ src/test/java/com/innoventes/jukebox/controller/AbstractControllerTest.java
@@ -1,12 +1,17 @@
 package com.innoventes.jukebox.controller;
 
+import com.innoventes.jukebox.controller.helper.MusicianHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.context.WebApplicationContext;
 
 @AutoConfigureMockMvc
 public class AbstractControllerTest {
+
+    @MockBean
+    protected MusicianHelper musicianHelper;
 
     @Autowired
     protected MockMvc mockMvc;
--- src/test/java/com/innoventes/jukebox/controller/MusicianControllerTest.java
+++ src/test/java/com/innoventes/jukebox/controller/MusicianControllerTest.java
@@ -1,10 +1,53 @@
 package com.innoventes.jukebox.controller;
 
 
+import com.innoventes.jukebox.constants.ApiConstants;
+import com.innoventes.jukebox.models.MusicianTypes;
+import com.innoventes.jukebox.models.request.MusicianRequest;
+import com.innoventes.jukebox.util.CommonUtils;
+import com.innoventes.jukebox.util.TestUtils;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.Objects;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 @SpringBootTest
 public class MusicianControllerTest extends AbstractControllerTest{
 
+    @Autowired
+    private TestAuthHelper testAuthHelper;
 
+    @Test
+    public void callAddMusicianWithoutAlbumId() throws Exception {
+        MusicianRequest musicianRequest = new MusicianRequest();
+        musicianRequest.setName("Test Musician");
+        musicianRequest.setType(MusicianTypes.POP.getMusicianType());
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .put(ApiConstants.MUSICIAN_BASE_PATH)
+                .header(TestUtils.AUTHORIZATION_HEADER, testAuthHelper.getAuthHeader("admin@jukebox.com", "password"))
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(Objects.requireNonNull(CommonUtils.convertToJson(musicianRequest)))
+                .accept(MediaType.APPLICATION_JSON);
+        mockMvc.perform(requestBuilder).andDo(print()).andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+    @Test
+    public void callAddMusicianWithEmptyName() throws Exception {
+        MusicianRequest request = new MusicianRequest();
+        request.setType(MusicianTypes.POP.getMusicianType());
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .put(ApiConstants.MUSICIAN_BASE_PATH)
+                .header(TestUtils.AUTHORIZATION_HEADER, testAuthHelper.getAuthHeader("admin@jukebox.com", "password"))
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(Objects.requireNonNull(CommonUtils.convertToJson(request)))
+                .accept(MediaType.APPLICATION_JSON);
+        mockMvc.perform(requestBuilder).andDo(print()).andExpect(MockMvcResultMatchers.status().isBadRequest());
+    }
 }
