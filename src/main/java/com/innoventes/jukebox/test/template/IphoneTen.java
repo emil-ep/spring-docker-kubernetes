5ec50c560ac6cfdd0e29dce2acb73d314860cb9b
Emil <emilpradeep@gmail.com>
2021-08-10 23:50:33 +0530

    added template pattern

---
+++ src/main/java/com/innoventes/jukebox/test/template/IphoneTen.java
@@ -0,0 +1,9 @@
+package com.innoventes.jukebox.test.template;
+
+public class IphoneTen extends Iphone{
+
+    @Override
+    public String getCameraResolution() {
+        return "12MP";
+    }
+}
---
+++ src/main/java/com/innoventes/jukebox/test/template/TemplateMain.java
@@ -0,0 +1,10 @@
+package com.innoventes.jukebox.test.template;
+
+public class TemplateMain {
+
+    public static void main(String[] args) {
+        IphoneEleven iphoneEleven = new IphoneEleven();
+        String result = iphoneEleven.processOrder();
+        System.out.println(result);
+    }
+}
