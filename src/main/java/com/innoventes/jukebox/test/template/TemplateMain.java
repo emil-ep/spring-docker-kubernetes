ae6ab5f9674aeb1f84479dae38852ce1ed30f4d1
Emil <emilpradeep@gmail.com>
2021-08-15 18:13:14 +0530

    added template pattern to interview

--- src/main/java/com/innoventes/jukebox/test/template/TemplateMain.java
+++ src/main/java/com/innoventes/jukebox/test/template/TemplateMain.java
@@ -6,5 +6,8 @@ public class TemplateMain {
         IphoneEleven iphoneEleven = new IphoneEleven();
         String result = iphoneEleven.processOrder();
         System.out.println(result);
+
+        Iphone iphone = new IphoneTen();
+        iphone.processOrder();
     }
 }
