c359147208afff4a05b555609cba9e750e6607c6
Emil <emilpradeep@gmail.com>
2021-08-27 22:40:16 +0530

    added leetcode questions

---
+++ src/main/java/com/innoventes/jukebox/test/leetcode/SumMeetsTarget.java
@@ -0,0 +1,18 @@
+package com.innoventes.jukebox.test.leetcode;
+
+public class SumMeetsTarget {
+
+
+    public static void main(String[] args) {
+
+        int[] array = new int[]{2,7,11,15};
+//        System.out.println(Solution.twoSum(array, 9));
+
+        int[] nums1 = new int[]{1,1};
+        int[] nums2 = new int[]{1,2};
+        System.out.println(Solution.findMedianSortedArrays(nums1, nums2));
+
+    }
+
+
+}
